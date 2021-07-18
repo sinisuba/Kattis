3498ac173ee5e6c38b2e5812c9a49e79f022c271
Siniša Subašić <68671543+sinisuba@users.noreply.github.com>
2021-07-19 00:20:36 +0200

    Optimizations

--- akcija.cpp
+++ akcija.cpp
@@ -21,21 +21,18 @@ int main()
         for (int i = 0; i < n; i += 3)
             for (int j = i; j < i + 3 - 1; j++)
                 ans += niz[j];
-
-        std::cout << ans;
-        return 0;
     }
     else
     {
-        int i = 0, j = 0;
+        int i = 0;
 
         for (i = 0; i < n - 3; i += 3)
-            for (j = i; j < i + 3 - 1; j++)
+            for (int j = i; j < i + 3 - 1; j++)
                 ans += niz[j];
 
         for (int x = i; x < n; x++)
             ans += niz[x];
-
-        std::cout << ans << std::endl;
     }
+
+    std::cout << ans;
 }
