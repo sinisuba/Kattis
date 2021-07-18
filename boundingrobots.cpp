26270a25458509b3d6ae58ec5e97300af5a42443
Siniša Subašić <68671543+sinisuba@users.noreply.github.com>
2021-07-19 00:24:29 +0200

    Optimizations

--- boundingrobots.cpp
+++ boundingrobots.cpp
@@ -12,38 +12,37 @@ int main()
     while (1)
     {
         rx = 0, ry = 0, ax = 0, ay = 0;
+
         std::cin >> w >> l;
-		
+
         if (w == 0 && l == 0)
             break;
 
         std::cin >> n;
-		
+
         for (int i = 0; i < n; ++i)
         {
             std::cin >> ch >> curr_move;
 
-            if (ch == 'u')
+            switch(ch)
             {
-                ax += curr_move;
-                rx += curr_move;
+                case 'u':
+                    ax += curr_move;
+                    rx += curr_move;
+                    break;
+                case 'd':
+                    ax -= curr_move;
+                    rx -= curr_move;
+                    break;
+                case 'l':
+                    ay -= curr_move;
+                    ry -= curr_move;
+                    break;
+                default:
+                    ay += curr_move;
+                    ry += curr_move;
             }
-            else if (ch == 'd')
-            {
-                ax -= curr_move;
-                rx -= curr_move;
-            }
-            else if (ch == 'l')
-            {
-                ay -= curr_move;
-                ry -= curr_move;
-            }
-            else
-            {
-                ay += curr_move;
-                ry += curr_move;
-            }
-			
+
             if (ax >= l)
                 ax = l - 1;
             if (ay >= w)
@@ -53,7 +52,7 @@ int main()
             if (ay < 0)
                 ay = 0;
         }
-		
+
         std::cout << "Robot thinks " << ry << " " << rx << std::endl;
         std::cout << "Actually at " << ay << " " << ax << std::endl;
         std::cout << std::endl;
