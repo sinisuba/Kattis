eea82aa81d5a6bfb561198f9e13a26cf920729b7
Siniša Subašić <68671543+sinisuba@users.noreply.github.com>
2021-07-18 22:04:48 +0200

    Optimizations

--- synchronizinglists.cpp
+++ synchronizinglists.cpp
@@ -1,35 +1,29 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <climits>
 
 int main() {
     int n;
+    
     while (std::cin >> n && n != 0)
     {
-        std::vector<int> prvi_niz(n);
-        
-        std::vector<int> drugi_niz(n);
-
-        int min = INT_MAX;
-        int* index_values = new int[n] {};
+        std::vector<int> prvi_niz(n), drugi_niz(n), index_values(n);
 
         for (int i = 0; i < n; i++)
             std::cin >> prvi_niz[i];
 
         std::vector<int> prvi_niz_copy(prvi_niz);
+        
+        std::sort(prvi_niz.begin(), prvi_niz.end());
 
         for (int i = 0; i < n; i++)
             std::cin >> drugi_niz[i];
 
-        std::sort(prvi_niz.begin(), prvi_niz.end());
-
         for (int i = 0; i < n; i++)
         {
-            int element_to_find = prvi_niz[i];
             for (int j = 0; j < n; j++)
             {
-                if (prvi_niz_copy[j] == element_to_find)
+                if (prvi_niz_copy[j] == prvi_niz[i])
                 {
                     index_values[i] = j;
                     break;
@@ -44,10 +38,9 @@ int main() {
         for (int i = 0; i < n; i++)
             rezultat[index_values[i]] = drugi_niz[i];
 
-        for (auto& i : rezultat)
+        for (int i : rezultat)
             std::cout << i << std::endl;
+            
         std::cout << std::endl;
-        
-        delete[] index_values;
     }
 }
